6a97cc1152b23d0761f957a1c2ba5afc37379fdc
Bilal Asif <bilalasif435@hotmail.com>
2022-06-23 12:05:43 +0500

    API Keys added to interact EtherScan, Added Named Accounts to be used in deployment scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,35 +6,55 @@ require("hardhat-gas-reporter");
 require("solidity-coverage");
 require("hardhat-deploy")
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async(taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners();
 
-    for (const account of accounts) {
-        console.log(account.address);
-    }
-});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+const RINNKBEY_RPC_URL = process.env.RINNKBEY_RPC_URL || "https://eth-rinkbey/example"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
 module.exports = {
-    solidity: "0.8.8",
+    solidity: {
+        compilers: [{
+                version: "0.8.8",
+            },
+            {
+                version: "0.6.6",
+            }
+        ],
+    },
+    defaultNetwork: "hardhat",
     networks: {
-        ropsten: {
-            url: process.env.ROPSTEN_URL || "",
-            accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        rinkeby: {
+            url: RINNKBEY_RPC_URL,
+            accounts: [
+                PRIVATE_KEY
+            ],
+            chainId: 4,
+            blockConfirmations: 6,
         },
+
     },
     gasReporter: {
-        enabled: process.env.REPORT_GAS !== undefined,
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
         currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: 'MATIC'
     },
     etherscan: {
-        apiKey: process.env.ETHERSCAN_API_KEY,
+        apiKey: ETHERSCAN_API_KEY,
     },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+
+        },
+        user: {
+            defulat: 1
+        }
+    }
 };
